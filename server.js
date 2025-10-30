e9259b2e22e2f7bd1687c6ba84cd0e10acd9e6b4
hcthisen <78961101+hcthisen@users.noreply.github.com>
2025-10-30 12:57:17 +0100

    Prevent caching of multipart signing responses

--- server.js
+++ server.js
@@ -374,6 +374,10 @@ app.get('/api/sign-part', asyncHandler(async (req, res) => {
     uploadId,
     partNumber: partNum
   });
+  res.set('Cache-Control', 'no-store, max-age=0, must-revalidate');
+  res.set('Pragma', 'no-cache');
+  res.set('Expires', '0');
+  res.removeHeader('ETag');
   res.json({ url });
 }));
 
